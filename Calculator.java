633dd4e9246d8a7993ffd5c4dc17f253682bd487
manogna1911 <minni.manognachowdary@gmail.com>
2021-02-22 10:42:05 +0530

    epamtask1

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+package com.epam.task;
+
+public class Calculator {
+	static int addition(int a,int b) {
+	 return a+b;
+	 }
+	 static int subtraction(int a,int b) {
+	 return a-b;
+	 }
+	 static int multiplication(int a,int b) {
+	 return a*b;
+	 }
+	 static int division(int a,int b) {
+	 return a/b;
+	 }
+
+
+}
